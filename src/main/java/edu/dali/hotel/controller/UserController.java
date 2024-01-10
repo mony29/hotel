95de4b2857d2b463c17add8456b2fcb55cfbeba9
Keo Mony <keomony929@gmail.com>
2024-01-11 01:36:11 +0800

    completed

---
+++ src/main/java/edu/dali/hotel/controller/UserController.java
@@ -0,0 +1,24 @@
+package edu.dali.hotel.controller;
+
+import edu.dali.hotel.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @RequestMapping("/login_view")
+    public String loginView() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String doLogin(@ModelAttribute("user") User user, ModelMap modelMap) {
+        modelMap.addAttribute("user", user);
+        return "redirect:/rooms/to/room-list-page";
+    }
+}
